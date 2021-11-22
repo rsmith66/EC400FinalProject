72cdb5bdf4fde37f2f2ec0d1230371bb9cd1e6ac
rsmith66 <75346297+rsmith66@users.noreply.github.com>
2021-11-22 16:57:32 -0500

    Add files via upload

---
+++ RyanScontroller.py
@@ -0,0 +1,64 @@
+import pystk
+
+
+def control(aim_point, current_vel):
+    """
+    Set the Action for the low-level controller
+    :param aim_point: Aim point, in screen coordinate frame [-1..1]
+    :param current_vel: Current velocity of the kart
+    :return: a pystk.Action (set acceleration, brake, steer, drift)
+    """
+    action = pystk.Action()
+    if(current_vel <= 20):          #increase acceleration till reach a constant velocity
+        action.acceleration = action.acceleration + 1
+    if(aim_point[0] > 0.2):
+        action.drift = True
+    if(aim_point[0] < -0.2):
+        action.drift = True
+    if(aim_point[0] <= 0.25 and aim_point[0] >= -0.25):      #if aim point is zero, boost
+        action.acceleration = action.acceleration + 10
+        action.steer = 0
+        action.nitro = True
+    if(aim_point[0] < 0):      #if aim point is negative, turn left
+        action.steer = -1
+    elif(aim_point[0] > 0):      #if aim point is positive, turn right
+        action.steer = 1
+    
+    
+    
+    
+    """
+    Your code here
+    Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
+    Hint: Use action.brake to True/False to brake (optionally)
+    Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
+    Hint: You may want to use action.drift=True for wide turns (it will turn faster)
+    """
+
+
+   
+
+    
+
+    return action
+
+
+
+if __name__ == '__main__':
+    from utils import PyTux
+    from argparse import ArgumentParser
+
+    def test_controller(args):
+        import numpy as np
+        pytux = PyTux()
+        for t in args.track:
+            steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
+            print(steps, how_far)
+        pytux.close()
+
+
+    parser = ArgumentParser()
+    parser.add_argument('track', nargs='+')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    test_controller(args)
